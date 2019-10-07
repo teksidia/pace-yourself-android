a2f747270258dabf7ab49fe8af9332e83e89b04a
Jamie Cox <46043201@ad.mmu.ac.uk>
2019-10-07 16:24:24 +0100

    More functionality

--- app/src/main/java/com/teksidia/paceyourself/DrinkActivity.java
+++ app/src/main/java/com/teksidia/paceyourself/DrinkActivity.java
@@ -4,22 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.temporal.ChronoField;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -27,12 +26,14 @@ public class DrinkActivity extends AppCompatActivity {
     public static final String TARGET_HOUR = "com.teksidia.paceyourself.TARGET_HOUR";
     public static final String TARGET_MINUTE = "com.teksidia.paceyourself.TARGET_MINUTE";
 
-    private TextView mSettings;
     private TextView mCountdown;
+    private CountDownTimer mCountdownTimer;
+    private LinearLayout mBeerContainer;
+    private Button mNextButton;
 
-    private int mDrinksConsumed = 0;
     private int mTargetAmount;
-    private Calendar mDateTimeTarget;
+    private long mSessionTimeInMillisecs;
+    private int mDrinksConsumed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,80 +42,131 @@ public class DrinkActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mCountdown = findViewById(R.id.text_countdown);
+        mBeerContainer = findViewById(R.id.linear_beers);
+        mNextButton = findViewById(R.id.button_next_drink);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                nextDrink();
-
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                nextDrink(false);
             }
         });
 
-        mSettings = findViewById(R.id.text_settings);
-        mCountdown = findViewById(R.id.text_countdown);
         Intent intent = getIntent();
         mTargetAmount = intent.getIntExtra(TARGET_AMOUNT, 0);
         int targetHour = intent.getIntExtra(TARGET_HOUR, 0);
-        int targetMinute = intent.getIntExtra(TARGET_HOUR, 0);
+        int targetMinute = intent.getIntExtra(TARGET_MINUTE, 0);
 
-        Calendar dateTimeNow = Calendar.getInstance();
-        mDateTimeTarget = Calendar.getInstance();
-        if(targetHour < dateTimeNow.get(Calendar.HOUR_OF_DAY)) {
-            mDateTimeTarget.add(Calendar.DAY_OF_YEAR, 1);
-        }
-        mDateTimeTarget.set(Calendar.HOUR_OF_DAY, targetHour);
-        mDateTimeTarget.set(Calendar.MINUTE, targetMinute);
+        mSessionTimeInMillisecs = setSessionTime(targetHour, targetMinute);
 
-        nextDrink();
+        nextDrink(true);
 
     }
 
-    private void nextDrink() {
+    private long setSessionTime(int targetHour, int targetMinute) {
+        Calendar dateTimeNow = Calendar.getInstance();
+        Calendar dateTimeTarget = Calendar.getInstance();
+        if(targetHour < dateTimeNow.get(Calendar.HOUR_OF_DAY)) {
+            dateTimeTarget.add(Calendar.DAY_OF_YEAR, 1);
+        }
+        dateTimeTarget.set(Calendar.HOUR_OF_DAY, targetHour);
+        dateTimeTarget.set(Calendar.MINUTE, targetMinute);
+        long now = dateTimeNow.getTimeInMillis();
+        long target = dateTimeTarget.getTimeInMillis();
+        return (target - now);
+    }
 
-        CountDownTimer timer;
+    private void nextDrink(boolean isStart) {
 
-        mDrinksConsumed += 1;
-
-        mSettings.setText(Integer.toString(mDrinksConsumed));
+        if(!isStart) {
+            mDrinksConsumed += 1;
+        }
 
         final int drinksRemaining = (mTargetAmount - mDrinksConsumed);
 
+        adjustBeerIndicator(drinksRemaining);
+
         if(drinksRemaining == 0) {
-            mCountdown.setText("Go home");
+            mCountdownTimer.cancel();
+            mCountdown.setText("Stop drinking");
+            mNextButton.setEnabled(false);
             return;
         }
 
         if(drinksRemaining == 1) {
-            // last drink!
+            mNextButton.setText("Complete Session");
         }
 
-        Calendar dateTimeNow = Calendar.getInstance();
-        long now = dateTimeNow.getTimeInMillis();
-        long target = mDateTimeTarget.getTimeInMillis();
-        long sessionTimeInMillisecs = (target - now);
-        long timePerDrink = sessionTimeInMillisecs / drinksRemaining;
+        long millisecsPerDrink = mSessionTimeInMillisecs / drinksRemaining;
 
-        timer = new CountDownTimer(timePerDrink, 1000) {
+        if(mCountdownTimer != null) {
+            mCountdownTimer.cancel();
+        }
 
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-            Date date = new Date();
+        mCountdownTimer = new CountDownTimer(millisecsPerDrink, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                date.setTime(millisUntilFinished);
-                mCountdown.setText(sdf.format(date));
-             }
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                int minutes = (int) ((millisUntilFinished / (1000*60)) % 60);
+                int hours   = (int) ((millisUntilFinished / (1000*60*60)) % 24);
+                mCountdown.setText(String.format("%s:%s:%s",
+                        String.format("%02d", hours), String.format("%02d", minutes), String.format("%02d", seconds)));
+            }
 
             public void onFinish() {
-                mCountdown.setText("You are good to go!");
+                mCountdown.setText("Time for your next drink!");
             }
         };
 
-        timer.start();
+        mCountdownTimer.start();
 
     }
 
+    private void adjustBeerIndicator(int drinksRemaining) {
+        mBeerContainer.removeAllViews();
+        int currentDrink = (mTargetAmount - drinksRemaining);
+        LinearLayout beerRow = new LinearLayout(this);
+        for(int i = 0; i < mTargetAmount; i++) {
+            if (i % 5 == 0) {
+                beerRow = new LinearLayout(this);
+                beerRow.setOrientation(LinearLayout.HORIZONTAL);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                mBeerContainer.addView(beerRow, layoutParams);
+            }
+            LinearLayout.LayoutParams layoutParams =
+                    new LinearLayout.LayoutParams((int) getResources().getDimension(R.dimen.beer_glass), (int) getResources().getDimension(R.dimen.beer_glass));
+            ImageView ii = new ImageView(this);
+            if(i > currentDrink) {
+                ii.setBackgroundResource(R.drawable.beerfull);
+            } else if(i == currentDrink) {
+                ii.setBackgroundResource(R.drawable.beerhalf);
+            } else {
+                ii.setBackgroundResource(R.drawable.beerempty);
+            }
+            ii.setLayoutParams(layoutParams);
+            beerRow.addView(ii);
+        }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_drink, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.reset) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/teksidia/paceyourself/MainActivity.java
+++ app/src/main/java/com/teksidia/paceyourself/MainActivity.java
@@ -4,15 +4,13 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -21,9 +19,10 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mTargetDrinks = 0;
+    private int mTargetDrinks = 4;
     private int mTargetHour = 0;
     private int mTargetMinute = 0;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +30,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab_start);
+
         final TextView finishTime = findViewById(R.id.txt_finish_time);
 
-        finishTime.setText("22:00");
+        Calendar currentTime = Calendar.getInstance();
+        mTargetHour = currentTime.get(Calendar.HOUR_OF_DAY) + 4;
+        mTargetMinute = currentTime.get(Calendar.MINUTE);
+
+        finishTime.setText(String.format("%02d", mTargetHour) + ":" + String.format("%02d", mTargetMinute));
 
         NumberPicker np = findViewById(R.id.np);
-        np.setMinValue(0);
+        np.setMinValue(2);
         np.setMaxValue(10);
+        np.setValue(mTargetDrinks);
         np.setWrapSelectorWheel(false);
         np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
@@ -47,7 +51,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        mStartButton = findViewById(R.id.button_start);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, DrinkActivity.class);
@@ -59,46 +65,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
         finishTime.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                Calendar currentTime = Calendar.getInstance();
-                int hour = currentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = currentTime.get(Calendar.MINUTE);
+            @Override
+            public void onClick(View v) {
+
                 TimePickerDialog mTimePicker;
 
                 mTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                @Override
-                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                    mTargetHour = selectedHour;
-                    mTargetMinute = selectedMinute;
-                    finishTime.setText(selectedHour + ":" + selectedMinute);
-                }
-                }, hour, minute, true);
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        mTargetHour = selectedHour;
+                        mTargetMinute = selectedMinute;
+                        finishTime.setText(String.format("%02d", selectedHour) + ":" + String.format("%02d", selectedMinute));
+                    }
+                }, mTargetHour, mTargetMinute, true);
                 mTimePicker.setTitle("Select Time");
                 mTimePicker.show();
             }
-          });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        });
     }
 }
